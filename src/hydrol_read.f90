1b3221310b3e0f76ffad60b43f958991e17019c8
fgeter <fgeter@gmail.com>
2025-12-19 09:21:33 -0700

    Refactor hydrol_read subroutine for improved readability and maintainability by renaming variables, removing commented-out code, and enhancing header processing logic.

--- src/hydrol_read.f90
+++ src/hydrol_read.f90
@@ -7,9 +7,7 @@
       
       implicit none
 
-      ! integer :: ithyd = 0            !none       |counter
-      character (len=80) :: titldum = ""!           |title of file
-      ! character (len=80) :: header = "" !           |header of file
+      character (len=80) :: titldum = ""!         |first line in file that generally is the title and it will be ignored.
       integer :: eof = 0              !           |end of file
       integer :: imax = 0             !none       |determine max number for array (imax) and total number in file
       logical :: i_exist              !none       |check to determine if file exists
@@ -17,18 +15,18 @@
       character(len=3100)         :: line
       integer, parameter :: max_cols = 100
       integer, parameter :: max_name_len = 30
-      character(len=max_name_len) :: headers(max_cols)
+      character(len=max_name_len) :: header_cols(max_cols)
       character(len=max_name_len) :: fields(max_cols)
       character(len=:), allocatable :: left_str
       integer                     :: i, num_data_row, num_header_cols, num_data_cols, num_skip_rows
       integer                     :: nfields
-      logical                     :: header_row
+      logical                     :: found_header_row
       
       
       eof = 0
       imax = 0
       nfields = 0
-      header_row = .false.
+      found_header_row = .false.
       
       !! read all data from hydrol.dat
       inquire (file=in_hyd%hydrol_hyd, exist=i_exist)
@@ -46,12 +44,6 @@
           else
             exit
           endif
-          ! read (107,*,iostat=eof) header
-          ! if (eof < 0) exit
-          ! do while (eof == 0)
-          !   read (107,*,iostat=eof) titldum
-          !   if (eof < 0) exit
-          !    imax = imax + 1
         end do
           
         do
@@ -59,59 +51,60 @@
           read(107, '(A)', iostat=eof) line
           if (eof /= 0) exit  ! EOF
           line = adjustl(trim(line))
-          call left_of_delim(line, '#', left_str)   !remove comments
-          if ( len(left_str) == 0) cycle
+          call left_of_delim(line, '#', left_str)         ! remove comments
+          if ( len(left_str) == 0) cycle                  ! skip empty lines
           line = left_str
-          if (.not. header_row) then
-            header_row = .true.
-            call split_line(line, fields, num_header_cols)
+          if (.not. found_header_row) then                 ! check to see if the header row has not yet been processed
+            found_header_row = .true.
+            call split_line(line, fields, num_header_cols) ! process header row into header columns
             cycle
           end if
-          call split_line(line, fields, num_data_cols)
-          ! Ignore row if the number data columns does not match the number of header columns
+          call split_line(line, fields, num_data_cols)     ! split data row into fields
+          ! Ignore datarow if the number data columns does not match the number of header columns
           if (num_header_cols /= num_data_cols) then
               cycle
           end if
           imax = imax + 1
         end do
+
         allocate (hyd_db(imax))
 
-
-        ! read (107,*,iostat=eof) titldum
-        ! if (eof < 0) exit
-        ! read (107,*,iostat=eof) header
-        ! if (eof < 0) exit
-                
-        ! do ithyd = 1, imax
-        !    read (107,*,iostat=eof) hyd_db(ithyd)            
-        !    if (eof < 0) exit
-        ! end do
-        ! exit
-
+        ! Rince and repeat to actually read in the data now that we know the size of the data object    
         rewind (107)
-        ! Read the first line and ignore it
-        read (107,*,iostat=eof) titldum
+        
+        read (107,*,iostat=eof) titldum ! Read the first line and ignore it 
 
-        header_row = .false.
+        found_header_row = .false.
         num_data_row = 0
         num_skip_rows = 1
         do
           read(107, '(A)', iostat=eof) line
           if (eof /= 0) exit  ! EOF
           line = adjustl(trim(line))
+
+          ! get portion of line left of comment delimiter '#'
           call left_of_delim(line, '#', left_str)
           line = left_str
+
+          ! skip empty lines
           if (len(left_str) == 0 ) then
               num_skip_rows = num_skip_rows + 1
-              cycle
+              cycle ! get next line
           endif
-          if (.not. header_row) then
-            call split_line(line, headers, num_header_cols)
+         
+          ! check to if the header row has not yet been processed
+          if (.not. found_header_row) then
+            ! process header row into header columns
+            call split_line(line, header_cols, num_header_cols)
             num_skip_rows = num_skip_rows + 1
-            header_row = .true.
-            cycle
+            found_header_row = .true.
+            cycle ! skip to next line
           end if
+
+          ! split data row into fields
           call split_line(line, fields, num_data_cols)
+          
+          ! check for correct number of columns and if incorrect skip row with warning
           if (num_data_cols /= num_header_cols) then
             num_skip_rows = num_skip_rows + 1
             write(9001,'(A,I3, A)') 'Warning: Row ', num_data_row + num_skip_rows, ' in hydrology.hyd has the wrong number of columns, skipping'
@@ -120,11 +113,12 @@
           end if
           num_data_row = num_data_row + 1
             
+          ! Assign data to hyd_db fields based on header column names
           do i = 1, num_data_cols
-            select case (to_lower(trim(adjustl(headers(i)))))
+            select case (to_lower(header_cols(i)))
   
             case ("name")
-                hyd_db(num_data_row)%name = trim(adjustl(fields(i)))
+                hyd_db(num_data_row)%name = fields(i)
             case ("lat_ttime")
                 read(fields(i), *) hyd_db(num_data_row)%lat_ttime
             case ("lat_sed")
@@ -154,8 +148,8 @@
             case ("latq_co")
                   read(fields(i), *) hyd_db(num_data_row)%latq_co
             case default
-                write(9001,'(2A)') 'Warning: unknown column header in hydrology.hyd, skipping: ', to_lower(trim(headers(i)))
-                print *, 'Warning: unknown column header in hydrology.hyd, skipping: ', to_lower(trim(headers(i)))
+                write(9001,'(2A)') 'Warning: unknown column header in hydrology.hyd, skipping: ', to_lower(trim(header_cols(i)))
+                print *, 'Warning: unknown column header in hydrology.hyd, skipping: ', to_lower(trim(header_cols(i)))
             end select
 
           end do
