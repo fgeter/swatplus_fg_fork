f884de0ce00929c643357ee2fd83e75d4be080de
fgeter <fgeter@gmail.com>
2026-01-06 14:37:01 -0700

    refactored hydro_read and cons_prac_read.f90 and added functions and subroutines to help with reading tabular data.

--- src/cons_prac_read.f90
+++ src/cons_prac_read.f90
@@ -3,50 +3,82 @@
       use input_file_module
       use maximum_data_module
       use landuse_data_module
+      use utils
       
       implicit none
-      
-      character (len=80) :: titldum = ""!           |title of file
-      character (len=80) :: header = "" !           |header of file
-      logical :: i_exist              !none       |check to determine if file exists
+      character (len=80) :: titldum = ""!         |first line in file that generally is the title and it will be ignored.
       integer :: eof = 0              !           |end of file
-      integer :: imax = 0             !           |
-      integer :: icp = 0              !none       |counter
+      integer :: imax = 0             !none       |determine max number for array (imax) and total number in file
+      logical :: i_exist              !none       |check to determine if file exists
+
+      character(MAX_NAME_LEN)       :: header_cols(MAX_TABLE_COLS) 
+      character(MAX_NAME_LEN)       :: data_fields(MAX_TABLE_COLS)
+      integer                       :: i, nrow, nheader_cols, ndata_cols, num_skip_rows
+      integer                       :: nfields
+      character(:), allocatable     :: sub_name !name of subroutine for data read warning messages       
+      
+      sub_name = "cons_prac_read"
       
       eof = 0
       imax = 0
+      nfields = 0
+      num_skip_rows = 0
       
     !! read all curve number data from cn.tbl
       inquire (file=in_lum%cons_prac_lum, exist=i_exist)
       if (.not. i_exist .or. in_lum%cons_prac_lum == "null") then
         allocate (cons_prac(0:0))
       else
-      do
         open (107,file=in_lum%cons_prac_lum)
-        read (107,*,iostat=eof) titldum
-        if (eof < 0) exit
-        read (107,*,iostat=eof) header
-        if (eof < 0) exit
-         do while (eof == 0)
-           read (107,*,iostat=eof) titldum
-           if (eof < 0) exit
-           imax = imax + 1
-         end do
-         
+        imax = num_data_lines_in_data_table(107)  !determine number of actual valid data lines in the file
+
         allocate (cons_prac(0:imax))
-        
+        if (imax == 0) then
+          db_mx%cons_prac = imax
+          close (107)
+          return
+        end if
+
+        ! Rinse and repeat to actually read the data in now that we know the size of the data object    
         rewind (107)
-        read (107,*,iostat=eof) titldum
-        if (eof < 0) exit
-        read (107,*,iostat=eof) header
-        if (eof < 0) exit
         
-        do icp = 1, imax
-          read (107,*,iostat=eof) cons_prac(icp)
-          if (eof < 0) exit
-        end do
-        exit
-      enddo
+        ! get the column headers
+        call get_data_table_header_columns(107, header_cols, nheader_cols, num_skip_rows, eof)
+
+        if (eof == 0) then   ! proceed if not at the end of the file.
+          nrow = 0
+          do
+            ! get a row of data
+            call get_data_table_row_fields(107, data_fields, ndata_cols, num_skip_rows, eof)
+            if (eof /= 0) exit  ! exit if at the end of the file.
+            
+            ! check for correct number of columns and if incorrect skip row with warning
+            if (ndata_cols /= nheader_cols) then
+              num_skip_rows = num_skip_rows + 1
+              write(9001,'(A,I3, 3A)') 'Warning: Row ', nrow + num_skip_rows, ' in ', sub_name, ' has the wrong number of columns, skipping'
+              print('(A,I3, 3A)'), 'Warning: Row ', nrow + num_skip_rows, ' in ', sub_name, ' has the wrong number of columns, skipping'
+              cycle
+            end if
+
+            nrow = nrow + 1
+              
+            ! Assign data to cons_prac fields based on header column names
+            do i = 1, ndata_cols
+              select case (to_lower(header_cols(i)))
+              case ("name")
+                  cons_prac(nrow)%name = data_fields(i)
+              case ("pfac")
+                  read(data_fields(i), *) cons_prac(nrow)%pfac
+              case ("sl_len_mx")
+                  read(data_fields(i), *) cons_prac(nrow)%sl_len_mx
+              case default
+                  write(9001,'(4A)') 'Warning: unknown column header in ', sub_name, ' skipping: ', to_lower(trim(header_cols(i)))
+                  print('(4A)'), 'Warning: unknown column header in ', sub_name, ' skipping: ', to_lower(trim(header_cols(i)))
+              end select
+
+            end do
+          enddo
+        endif
       endif
       
       db_mx%cons_prac = imax
--- src/hydrol_read.f90
+++ src/hydrol_read.f90
@@ -12,148 +12,96 @@
       integer :: imax = 0             !none       |determine max number for array (imax) and total number in file
       logical :: i_exist              !none       |check to determine if file exists
 
-      character(len=3100)         :: line
-      integer, parameter :: max_cols = 100
-      integer, parameter :: max_name_len = 30
-      character(len=max_name_len) :: header_cols(max_cols)
-      character(len=max_name_len) :: fields(max_cols)
-      character(len=:), allocatable :: left_str
-      integer                     :: i, num_data_row, num_header_cols, num_data_cols, num_skip_rows
-      integer                     :: nfields
-      logical                     :: found_header_row
+      character(MAX_NAME_LEN)       :: header_cols(MAX_TABLE_COLS) 
+      character(MAX_NAME_LEN)       :: data_fields(MAX_TABLE_COLS)
+      integer                       :: i, nrow, nheader_cols, ndata_cols, num_skip_rows
+      integer                       :: nfields
+      character(:), allocatable     :: sub_name !name of subroutine for data read warning messages       
       
-      
+      sub_name = "hydrol_read"
+
       eof = 0
       imax = 0
       nfields = 0
-      found_header_row = .false.
+      num_skip_rows = 0
       
       !! read all data from hydrol.dat
       inquire (file=in_hyd%hydrol_hyd, exist=i_exist)
       if (.not. i_exist .or. in_hyd%hydrol_hyd == "null") then
         allocate (hyd_db(0:0))
       else
-        do
-          ! Read and ignore first line
-          open (107,file=in_hyd%hydrol_hyd)
-          read (107,*,iostat=eof) titldum
-          if (eof < 0) then 
-            db_mx%hyd = imax
-            allocate (hyd_db(0:0))
-            return
-          else
-            exit
-          endif
-        end do
-          
-        do
-          ! Determine the size of hyd_db and allocate it. 
-          read(107, '(A)', iostat=eof) line
-          if (eof /= 0) exit  ! EOF
-          line = adjustl(trim(line))
-          call left_of_delim(line, '#', left_str)         ! remove comments
-          if ( len(left_str) == 0) cycle                  ! skip empty lines
-          line = left_str
-          if (.not. found_header_row) then                 ! check to see if the header row has not yet been processed
-            found_header_row = .true.
-            call split_line(line, fields, num_header_cols) ! process header row into header columns
-            cycle
-          end if
-          call split_line(line, fields, num_data_cols)     ! split data row into fields
-          ! Ignore datarow if the number data columns does not match the number of header columns
-          if (num_header_cols /= num_data_cols) then
-              cycle
-          end if
-          imax = imax + 1
-        end do
+        open (107,file=in_hyd%hydrol_hyd)
+        imax = num_data_lines_in_data_table(107)
 
         allocate (hyd_db(imax))
+        if (imax == 0) then
+          db_mx%hyd = imax
+          close (107)
+          return
+        end if
 
-        ! Rince and repeat to actually read in the data now that we know the size of the data object    
+        ! Rinse and repeat to actually read in the data now that we know the size of the data object    
         rewind (107)
         
-        read (107,*,iostat=eof) titldum ! Read the first line and ignore it 
-
-        found_header_row = .false.
-        num_data_row = 0
-        num_skip_rows = 1
-        do
-          read(107, '(A)', iostat=eof) line
-          if (eof /= 0) exit  ! EOF
-          line = adjustl(trim(line))
-
-          ! get portion of line left of comment delimiter '#'
-          call left_of_delim(line, '#', left_str)
-          line = left_str
-
-          ! skip empty lines
-          if (len(left_str) == 0 ) then
-              num_skip_rows = num_skip_rows + 1
-              cycle ! get next line
-          endif
-         
-          ! check to if the header row has not yet been processed
-          if (.not. found_header_row) then
-            ! process header row into header columns
-            call split_line(line, header_cols, num_header_cols)
-            num_skip_rows = num_skip_rows + 1
-            found_header_row = .true.
-            cycle ! skip to next line
-          end if
-
-          ! split data row into fields
-          call split_line(line, fields, num_data_cols)
-          
-          ! check for correct number of columns and if incorrect skip row with warning
-          if (num_data_cols /= num_header_cols) then
-            num_skip_rows = num_skip_rows + 1
-            write(9001,'(A,I3, A)') 'Warning: Row ', num_data_row + num_skip_rows, ' in hydrology.hyd has the wrong number of columns, skipping'
-            print('(A,I3, A)'), 'Warning: Row ', num_data_row + num_skip_rows, ' in hydrology.hyd has the wrong number of columns, skipping'
-            cycle
-          end if
-          num_data_row = num_data_row + 1
+        call get_data_table_header_columns(107, header_cols, nheader_cols, num_skip_rows, eof)
+        if (eof == 0) then
+          nrow = 0
+          do
+            call get_data_table_row_fields(107, data_fields, ndata_cols, num_skip_rows, eof)
+            if (eof /= 0) exit  ! EOF
             
-          ! Assign data to hyd_db fields based on header column names
-          do i = 1, num_data_cols
-            select case (to_lower(header_cols(i)))
-  
-            case ("name")
-                hyd_db(num_data_row)%name = fields(i)
-            case ("lat_ttime")
-                read(fields(i), *) hyd_db(num_data_row)%lat_ttime
-            case ("lat_sed")
-                read(fields(i), *) hyd_db(num_data_row)%lat_sed
-            case ("canmx")
-                read(fields(i), *) hyd_db(num_data_row)%canmx
-            case ("esco")
-                read(fields(i), *) hyd_db(num_data_row)%esco
-            case ("epco")
-                read(fields(i), *) hyd_db(num_data_row)%epco
-            case ("erorgn")
-                read(fields(i), *) hyd_db(num_data_row)%erorgn
-            case ("erorgp")
-                read(fields(i), *) hyd_db(num_data_row)%erorgp
-            case ("cn3_swf")
-                read(fields(i), *) hyd_db(num_data_row)%cn3_swf
-            case ("biomix")
-                read(fields(i), *) hyd_db(num_data_row)%biomix
-            case ("perco")
-                read(fields(i), *) hyd_db(num_data_row)%perco
-            case ("lat_orgn")
-                read(fields(i), *) hyd_db(num_data_row)%lat_orgn
-            case ("lat_orgp")
-                read(fields(i), *) hyd_db(num_data_row)%lat_orgp
-            case ("pet_co")
-                read(fields(i), *) hyd_db(num_data_row)%pet_co
-            case ("latq_co")
-                  read(fields(i), *) hyd_db(num_data_row)%latq_co
-            case default
-                write(9001,'(2A)') 'Warning: unknown column header in hydrology.hyd, skipping: ', to_lower(trim(header_cols(i)))
-                print *, 'Warning: unknown column header in hydrology.hyd, skipping: ', to_lower(trim(header_cols(i)))
-            end select
+            ! check for correct number of columns and if incorrect skip row with warning
+            if (ndata_cols /= nheader_cols) then
+              num_skip_rows = num_skip_rows + 1
+              write(9001,'(A,I3, 3A)') 'Warning: Row ', nrow + num_skip_rows, ' in ', sub_name, ' has the wrong number of columns, skipping'
+              print('(A,I3, 3A)'), 'Warning: Row ', nrow + num_skip_rows, ' in ', sub_name, ' has the wrong number of columns, skipping'
+              cycle
+            end if
 
-          end do
-        enddo
+            nrow = nrow + 1
+              
+            ! Assign data to hyd_db fields based on header column names
+            do i = 1, ndata_cols
+              select case (to_lower(header_cols(i)))
+    
+              case ("name")
+                  hyd_db(nrow)%name = data_fields(i)
+              case ("lat_ttime")
+                  read(data_fields(i), *) hyd_db(nrow)%lat_ttime
+              case ("lat_sed")
+                  read(data_fields(i), *) hyd_db(nrow)%lat_sed
+              case ("canmx")
+                  read(data_fields(i), *) hyd_db(nrow)%canmx
+              case ("esco")
+                  read(data_fields(i), *) hyd_db(nrow)%esco
+              case ("epco")
+                  read(data_fields(i), *) hyd_db(nrow)%epco
+              case ("erorgn")
+                  read(data_fields(i), *) hyd_db(nrow)%erorgn
+              case ("erorgp")
+                  read(data_fields(i), *) hyd_db(nrow)%erorgp
+              case ("cn3_swf")
+                  read(data_fields(i), *) hyd_db(nrow)%cn3_swf
+              case ("biomix")
+                  read(data_fields(i), *) hyd_db(nrow)%biomix
+              case ("perco")
+                  read(data_fields(i), *) hyd_db(nrow)%perco
+              case ("lat_orgn")
+                  read(data_fields(i), *) hyd_db(nrow)%lat_orgn
+              case ("lat_orgp")
+                  read(data_fields(i), *) hyd_db(nrow)%lat_orgp
+              case ("pet_co")
+                  read(data_fields(i), *) hyd_db(nrow)%pet_co
+              case ("latq_co")
+                    read(data_fields(i), *) hyd_db(nrow)%latq_co
+              case default
+                  write(9001,'(4A)') 'Warning: unknown column header in ', sub_name, ' skipping: ', to_lower(trim(header_cols(i)))
+                  print('(4A)'), 'Warning: unknown column header in ', sub_name, ' skipping: ', to_lower(trim(header_cols(i)))
+              end select
+
+            end do
+          enddo
+        endif
       endif
       close (107)
  
