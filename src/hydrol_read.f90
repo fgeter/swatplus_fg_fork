7dd19a896b1d3d98783724227e0ff8892815eb63
fgeter <fgeter@gmail.com>
2026-01-09 21:44:38 -0700

    Add init_tblr_vars subroutine to initialize table reading variables to 0 or null  in cons_prac_read and hydrol_read.

--- src/hydrol_read.f90
+++ src/hydrol_read.f90
@@ -12,6 +12,8 @@ integer :: imax = 0       ! number of elements to be allocated
 logical :: i_exist        ! true if file exists
 integer :: i
 
+call init_tblr_vars()  ! initialize tblr variables to zero and arrays to null
+
 tblr%sub_name = "hyrol_read"
 tblr%unit = 107
 
@@ -27,7 +29,6 @@ else
     ! get the column headers
     call get_header_columns(eof)
     if (eof == 0) then
-      if (allocated(tblr%col_okay)) deallocate(tblr%col_okay)
       allocate (tblr%col_okay(tblr%ncols))
       tblr%col_okay = .true.
       
