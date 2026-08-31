1e195d714aa2f7ab8a28c1c2737da74ecef97930
fgeter <fgeter@gmail.com>
2026-08-31 10:22:39 -0600

    Added soil_test.sol

---
+++ refdata/Ames_sub1/soil_test.sol
@@ -0,0 +1,6 @@
+soil_test.sol: Ames_sub1 measured soil test results by layer (read when codes.bsn carbon = 1)
+  soil_name             depth_mm          bd         cbn        sand        silt        clay
+  soil_01-h1               150.0         2.0         4.0        30.0        30.0        40.0
+  soil_01-h1               250.0         1.5         3.0        25.0        35.0        40.0
+  soil_03-h3               125.0         1.6         3.0        40.0        30.0        30.0
+  soil_03-h3               150.0         2.0         3.0        15.0        45.0        40.0
